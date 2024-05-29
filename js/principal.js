e5f0a81d92ba8fbed1c7feb2089070692b8fd22f
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-29 14:38:54 -0300

    Adicionando for para replicar a validação e o cálculo do IMC para todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,24 +1,29 @@
 //document query selector
 var titulo = (document.querySelector('.titulo'));
 titulo.textContent = 'Lucas Nutricionista';
-//seleciona id primeiro paciente
-var paciente = document.querySelector('#primeiro-paciente');
-//seleciona peso
-var tdPeso = paciente.querySelector('.info-peso');
-var peso = tdPeso.textContent;
-//seleciona altura
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
-//calcula imc e altera classe imc
-var tdImc = paciente.querySelector('.info-imc');
+////document query selector all
+var pacientes = (document.querySelectorAll('.paciente'));
+for (var contador = 0;contador <= pacientes.length; contador++) {
+    console.log(pacientes[contador]);
 
-var pesoValido = true;
-var alturaValida = true;
+    var paciente = pacientes[contador];
+    //seleciona peso
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    //seleciona altura
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+    //calcula imc e altera classe imc
+    var tdImc = paciente.querySelector('.info-imc');
+
+    var pesoValido = true;
+    var alturaValida = true;
 
     if (peso <= 0 || peso >= 1000){
         console.log("Peso inválido");
         pesoValido = false;
         tdImc.textContent = 'Peso Inválido!';
+        paciente.style.color = "red";
     }
     if (altura <= 0 || altura >= 3.00 ){
         console.log("Altura inválida");
@@ -28,5 +33,6 @@ var alturaValida = true;
 
     if (alturaValida && pesoValido) {
         var imc = peso / (altura * altura);
-        tdImc.textContent = imc;    
+        tdImc.textContent = imc.toFixed(2);    
+    }
     }
