ec4b6abbf9655c8a8a04459620e76f770e7101c4
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-29 15:23:15 -0300

    Pequena Refatoração

--- js/principal.js
+++ js/principal.js
@@ -3,12 +3,12 @@ var titulo = (document.querySelector('.titulo'));
 titulo.textContent = 'Lucas Nutricionista';
 ////document query selector all
 var pacientes = (document.querySelectorAll('.paciente'));
-for (var contador = 0;contador <= pacientes.length; contador++) {
+for (var contador = 0;contador < pacientes.length; contador++) {
     console.log(pacientes[contador]);
 
     var paciente = pacientes[contador];
     //seleciona peso
-    var tdPeso = paciente.querySelector('.info-peso');
+    var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
     //seleciona altura
     var tdAltura = paciente.querySelector('.info-altura');
@@ -27,13 +27,13 @@ for (var contador = 0;contador <= pacientes.length; contador++) {
     }
     if (altura <= 0 || altura >= 3.00 ){
         console.log("Altura inválida");
-        pesoValido = false;
+        alturaValida = false;
         tdImc.textContent = 'Altura Inválido!';
         paciente.classList.add("paciente-invalido");
     }
 
     if (alturaValida && pesoValido) {
         var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);    
+        tdImc.textContent = imc.toFixed(2);  
     }
     }
