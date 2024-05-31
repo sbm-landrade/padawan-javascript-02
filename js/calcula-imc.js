03317ebf0755ca3aa08a3af8b99232b24357a5bc
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-31 13:17:49 -0300

    Criando função calcula-imc

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -33,11 +33,16 @@ for (var contador = 0;contador < pacientes.length; contador++) {
     }
 
     if (alturaValida && pesoValido) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);  
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;  
     }
 }
 
+function calculaImc(peso, altura) {
+    var imc = 0;
 
+    imc = peso / (altura * altura);
 
+    return imc.toFixed(2);
+}
 
