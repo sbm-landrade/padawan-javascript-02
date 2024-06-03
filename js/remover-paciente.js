f6002e2703683f57ae3ca96fd620725931710831
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-03 13:20:37 -0300

    Removendo pacientes

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,5 +3,6 @@ var pacientes = document.querySelectorAll(".paciente");
 pacientes.forEach(function(paciente) {
     paciente.addEventListener("dblclick", function() {
         console.log(" Fui cliclado com um duplo click");
+        this.remove();//ele não remove novos pacientes
     });
 });
