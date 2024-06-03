db978d6b7084599860d5efeb709569448eb1d72b
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-03 15:38:38 -0300

    Formatando dados JSON

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,1 +1,19 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
+
+    xhr.addEventListener("load", function() {
+        var resposta = xhr.responseText;
+        console.log(resposta);
+        console.log(typeof resposta);
+
+        var pacientes = JSON.parse(resposta);
+        console.log(pacientes);
+        console.log(typeof pacientes);
+    });
+
+    xhr.send();
+});
