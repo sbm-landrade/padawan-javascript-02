c1bc0eb163487c8f67df400c1fc97f3378d96540
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-03 16:03:27 -0300

    Capturando erros de requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,19 +6,19 @@ botaoAdicionar.addEventListener("click", function() {
     xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
 
     xhr.addEventListener("load", function() {
+        var erroAjax = document.querySelector("#erro-ajax");
 
-        if (xhr.status ==200){
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
             var resposta = xhr.responseText;
             var pacientes = JSON.parse(resposta);
-            
+
             pacientes.forEach(function(paciente) {
                 adicionaPacienteNaTabela(paciente);
             });
-        }else{
-            console.log(xhr.status);
-            console.log(xhr.responseText);
+        } else {
+            erroAjax.classList.remove("invisivel");
         }
-       
     });
 
     xhr.send();
